80b850ebedc31adc185bd2aa4a002d4f19f342c1
Valter Machado <valtermachadojr@gmail.com>
2020-02-15 12:43:03 -0300

    create project

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  "extends": "airbnb",
+  "plugins": [
+    "jsx-a11y",
+    "import"
+  ],
+  "env": {
+    "browser": true,
+    "es6": true,
+  },
+  "rules": {
+    "semi": 0,
+    "no-undef": "off",
+    "radix": "off",
+    "class-methods-use-this": "off",
+    "object-curly-newline": "off",
+    "import/no-unresolved": "off",
+    "react/no-array-index-key": 0,
+    "space-before-function-paren": 0,
+    "jsx-a11y/no-static-element-interactions": "off",
+    "jsx-a11y/anchor-is-valid": 0,
+    "import/prefer-default-export": false,
+    "no-param-reassign": "off",
+    "linebreak-style": 0,
+    "operator-linebreak": "off"
+  }
+}
