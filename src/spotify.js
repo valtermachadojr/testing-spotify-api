30f3cfb7873e932a5e061f4f36d289e9939bc8a7
Valter Machado <valtermachadojr@gmail.com>
2020-02-16 12:15:00 -0300

    remove invalid token

--- src/spotify.js
+++ src/spotify.js
@@ -1,7 +1,7 @@
 import SpotifyWrapper from 'spotify-wrapper'
 
 const spotify = new SpotifyWrapper({
-  token: 'BQDeZQMhsVjSdM0R55olf6gnRM-EVnjyeJ5g7NVIBC9T8CiGgnp5loJ8-nZo4UR7aGDlInunUKdOtTx1m-Q7jHqLUc5r0xlkJ8QKUFLLqB5-aoE1ZVZfvoYhpimCs6KcAO67D1POZuXMBWsc2IMwtKN1WGoZwFo',
+  token: 'YOUR_TOKEN_HERE',
 })
 
 export default spotify
